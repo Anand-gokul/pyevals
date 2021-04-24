eca7003659a34c6b188721b53599c19902c59bfe
Anand-gokul <69074974+Anand-gokul@users.noreply.github.com>
2021-04-24 23:18:40 +0530

    Update RegressionMetrics.py

--- pyevals/RegressionMetrics.py
+++ pyevals/RegressionMetrics.py
@@ -2,52 +2,7 @@ from pyevals.utils import *
 import pandas as pd
 from sklearn.metrics import f1_score
 from pyevals.exceptions import *
-
-
-
-def rsquared_score(actual, predicted):
-    """
-    :Params:
-    --------
-    actual: Input your y_test
-    predicted: Input your y_pred
-    :Returns:
-    ---------
-    returns your root mean square error
-    Examples
-    --------
-    >>> actual = [3, 0.5, 2, 7]
-    >>> predicted = [2.5, 0.3, 2, 8]
-    >>> rsquared_score(actual, predicted)
-    0.93530997
-    """
-    # Declaring the exceptions.
-
-    if len(actual) == 0:
-        raise PyEvalsValueError('There are currently no values in actual.')
-    if len(predicted) == 0:
-        raise PyEvalsValueError('There are currently no values in predicted.')
-    # for i in range(len(actual)):
-    #     if (actual[i] < 0).any() or (predicted[i] < 0)():
-    #         raise PyEvalsValueError('There cannot be any negative values to calculate r2_score')
-
-    actual = check_type(actual)
-    predicted = check_type(predicted)
-    type_actual, actual, predicted = input_regression(actual, predicted)
-    equal_lengths(actual, predicted)
-    actual, predicted = np.array(actual), np.array(predicted)
-    sse = ((actual - predicted) ** 2).sum(dtype=float)
-    sse_nonzero = sse != 0  # To check for only non-zero values as input
-    sst = ((actual - np.average(actual)) ** 2).sum(dtype=float)
-    sst_nonzero = sst != 0  # To check for only non-zero values as input
-    nonzero_values = sse_nonzero and sst_nonzero
-    output = 1 - (sse[nonzero_values] / sst[nonzero_values])
-
-    # Condition if either the values are equal to zero, it returns 1.0 else returns the actual score.
-    if not np.any(sse_nonzero):
-        if not np.any(sst_nonzero):
-            return 1.0
-    return output
+from sklearn.metrics import r2_score
 
 
 def rmse(actual, predicted):
@@ -131,9 +86,6 @@ def adj_rsquared_score(X_train, actual, predicted, rsquared_score):
         raise PyEvalsValueError('There are currently no values in actual.')
     if len(predicted) == 0:
         raise PyEvalsValueError('There are currently no values in predicted.')
-    for i in range(len(actual)):
-        if (actual[i] < 0) or (predicted[i] < 0):
-            raise PyEvalsValueError('There cannot be any negative values to calculate r2_score')
 
     actual = check_type(actual)
     predicted = check_type(predicted)
@@ -143,7 +95,7 @@ def adj_rsquared_score(X_train, actual, predicted, rsquared_score):
     df = pd.DataFrame(X_train)
     n = len(X_train)
     p = len(df.columns)
-    output = (1 - ((1 - rsquared_score(actual, predicted)) * (n - 1) / (n - p - 1)))
+    output = (1 - ((1 - r2_score(actual, predicted)) * (n - 1) / (n - p - 1)))
     return output
 
 
